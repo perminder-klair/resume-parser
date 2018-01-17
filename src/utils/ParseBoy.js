f51065f6c553a3be4d0396d852530019073c7c64
Parminder Klair <perminder.klair@gmail.com>
2018-01-17 11:12:42 +0530

    lint code

--- src/utils/ParseBoy.js
+++ src/utils/ParseBoy.js
@@ -1,25 +1,22 @@
 'use strict';
-const _          = require('underscore'),
-      processing = require('./libs/processing'),
-      parser     = require('./libs/parser'),
-      logger     = require('tracer').colorConsole();
+const _ = require('underscore'),
+  processing = require('./libs/processing'),
+  parser = require('./libs/parser'),
+  logger = require('tracer').colorConsole();
 
 /**
  *
  * @constructor
  */
-function ParseBoy() {
-
-}
-
+function ParseBoy() {}
 
 /**
  *
  * @param PreparedFile
  * @param cbGetResume
  */
-ParseBoy.prototype.parseFile = function (PreparedFile, cbGetResume) {
-  logger.trace('I\'m working with "' + PreparedFile.name + '" now')
+ParseBoy.prototype.parseFile = function(PreparedFile, cbGetResume) {
+  logger.trace('I\'m working with "' + PreparedFile.name + '" now');
   parser.parse(PreparedFile, cbGetResume);
 };
 
@@ -30,7 +27,12 @@ ParseBoy.prototype.parseFile = function (PreparedFile, cbGetResume) {
  * @param path
  * @param cbOnSaved
  */
-ParseBoy.prototype.storeResume = function (PreparedFile, Resume, path, cbOnSaved) {
+ParseBoy.prototype.storeResume = function(
+  PreparedFile,
+  Resume,
+  path,
+  cbOnSaved
+) {
   PreparedFile.addResume(Resume);
 
   if (!_.isFunction(cbOnSaved)) {
--- src/utils/dictionary.js
+++ src/utils/dictionary.js
@@ -1,5 +1,5 @@
-var request = require("request");
-var cheerio = require("cheerio");
+var request = require('request');
+var cheerio = require('cheerio');
 var _ = require('underscore');
 
 module.exports = {
@@ -16,138 +16,157 @@ module.exports = {
     links: ['links'],
     contacts: ['contacts'],
     positions: ['positions', 'position'],
-    profiles: ['profiles', 'social connect', 'social-profiles', 'social profiles'],
+    profiles: [
+      'profiles',
+      'social connect',
+      'social-profiles',
+      'social profiles',
+    ],
     awards: ['awards'],
     honors: ['honors'],
     additional: ['additional'],
     certification: ['certification', 'certifications'],
-    interests: ['interests']
+    interests: ['interests'],
   },
   profiles: [
-    ['github.com', function(url, Resume, profilesWatcher) {
-      download(url, function(data, err) {
-        if (data) {
-          var $ = cheerio.load(data),
-            fullName = $('.vcard-fullname').text(),
-            location = $('.octicon-location').parent().text(),
-            mail = $('.octicon-mail').parent().text(),
-            link = $('.octicon-link').parent().text(),
-            clock = $('.octicon-clock').parent().text(),
-            company = $('.octicon-organization').parent().text();
+    [
+      'github.com',
+      function(url, Resume, profilesWatcher) {
+        download(url, function(data, err) {
+          if (data) {
+            var $ = cheerio.load(data),
+              fullName = $('.vcard-fullname').text(),
+              location = $('.octicon-location')
+                .parent()
+                .text(),
+              mail = $('.octicon-mail')
+                .parent()
+                .text(),
+              link = $('.octicon-link')
+                .parent()
+                .text(),
+              clock = $('.octicon-clock')
+                .parent()
+                .text(),
+              company = $('.octicon-organization')
+                .parent()
+                .text();
 
-          Resume.addObject('github', {
-            name: fullName,
-            location: location,
-            email: mail,
-            link: link,
-            joined: clock,
-            company: company
-          });
-        } else {
-          return console.log(err);
-        }
-        //profilesInProgress--;
-        profilesWatcher.inProgress--;
-      });
-    }],
-    ['linkedin.com', function(url, Resume, profilesWatcher) {
-      download(url, function(data, err) {
-        if (data) {
-          var $ = cheerio.load(data),
-            linkedData = {
-              positions: {
-                past: [],
-                current: {}
+            Resume.addObject('github', {
+              name: fullName,
+              location: location,
+              email: mail,
+              link: link,
+              joined: clock,
+              company: company,
+            });
+          } else {
+            return console.log(err);
+          }
+          //profilesInProgress--;
+          profilesWatcher.inProgress--;
+        });
+      },
+    ],
+    [
+      'linkedin.com',
+      function(url, Resume, profilesWatcher) {
+        download(url, function(data, err) {
+          if (data) {
+            var $ = cheerio.load(data),
+              linkedData = {
+                positions: {
+                  past: [],
+                  current: {},
+                },
+                languages: [],
+                skills: [],
+                educations: [],
+                volunteering: [],
+                volunteeringOpportunities: [],
               },
-              languages: [],
-              skills: [],
-              educations: [],
-              volunteering: [],
-              volunteeringOpportunities: []
-            },
-            $pastPositions = $('.past-position'),
-            $currentPosition = $('.current-position'),
-            $languages = $('#languages-view .section-item > h4 > span'),
-            $skills = $('.skills-section .skill-pill .endorse-item-name-text'),
-            $educations = $('.education'),
-            $volunteeringListing = $('ul.volunteering-listing > li'),
-            $volunteeringOpportunities = $('ul.volunteering-opportunities > li');
+              $pastPositions = $('.past-position'),
+              $currentPosition = $('.current-position'),
+              $languages = $('#languages-view .section-item > h4 > span'),
+              $skills = $(
+                '.skills-section .skill-pill .endorse-item-name-text'
+              ),
+              $educations = $('.education'),
+              $volunteeringListing = $('ul.volunteering-listing > li'),
+              $volunteeringOpportunities = $(
+                'ul.volunteering-opportunities > li'
+              );
 
-          linkedData.summary = $('#summary-item .summary').text();
-          linkedData.name = $('.full-name').text();
-          // current position
-          linkedData.positions.current = {
-            title: $currentPosition.find('header > h4').text(),
-            company: $currentPosition.find('header > h5').text(),
-            description: $currentPosition.find('p.description').text(),
-            period: $currentPosition.find('.experience-date-locale').text()
-          };
-          // past positions
-          _.forEach($pastPositions, function(pastPosition) {
-            var $pastPosition = $(pastPosition);
-            linkedData.positions.past.push({
-              title: $pastPosition.find('header > h4').text(),
-              company: $pastPosition.find('header > h5').text(),
-              description: $pastPosition.find('p.description').text(),
-              period: $pastPosition.find('.experience-date-locale').text()
+            linkedData.summary = $('#summary-item .summary').text();
+            linkedData.name = $('.full-name').text();
+            // current position
+            linkedData.positions.current = {
+              title: $currentPosition.find('header > h4').text(),
+              company: $currentPosition.find('header > h5').text(),
+              description: $currentPosition.find('p.description').text(),
+              period: $currentPosition.find('.experience-date-locale').text(),
+            };
+            // past positions
+            _.forEach($pastPositions, function(pastPosition) {
+              var $pastPosition = $(pastPosition);
+              linkedData.positions.past.push({
+                title: $pastPosition.find('header > h4').text(),
+                company: $pastPosition.find('header > h5').text(),
+                description: $pastPosition.find('p.description').text(),
+                period: $pastPosition.find('.experience-date-locale').text(),
+              });
             });
-          });
-          _.forEach($languages, function(language) {
-            linkedData.languages.push($(language).text());
-          });
-          _.forEach($skills, function(skill) {
-            linkedData.skills.push($(skill).text());
-          });
-          _.forEach($educations, function(education) {
-            var $education = $(education);
-            linkedData.educations.push({
-              title: $education.find('header > h4').text(),
-              major: $education.find('header > h5').text(),
-              date: $education.find('.education-date').text()
+            _.forEach($languages, function(language) {
+              linkedData.languages.push($(language).text());
             });
-          });
-          _.forEach($volunteeringListing, function(volunteering) {
-            linkedData.volunteering.push($(volunteering).text());
-          });
-          _.forEach($volunteeringOpportunities, function(volunteering) {
-            linkedData.volunteeringOpportunities.push($(volunteering).text());
-          });
+            _.forEach($skills, function(skill) {
+              linkedData.skills.push($(skill).text());
+            });
+            _.forEach($educations, function(education) {
+              var $education = $(education);
+              linkedData.educations.push({
+                title: $education.find('header > h4').text(),
+                major: $education.find('header > h5').text(),
+                date: $education.find('.education-date').text(),
+              });
+            });
+            _.forEach($volunteeringListing, function(volunteering) {
+              linkedData.volunteering.push($(volunteering).text());
+            });
+            _.forEach($volunteeringOpportunities, function(volunteering) {
+              linkedData.volunteeringOpportunities.push($(volunteering).text());
+            });
 
-          Resume.addObject('linkedin', linkedData);
-        } else {
-          return console.log(err);
-        }
-        profilesWatcher.inProgress--;
-      });
-    }],
+            Resume.addObject('linkedin', linkedData);
+          } else {
+            return console.log(err);
+          }
+          profilesWatcher.inProgress--;
+        });
+      },
+    ],
     'facebook.com',
     'bitbucket.org',
-    'stackoverflow.com'
+    'stackoverflow.com',
   ],
   inline: {
     //address: 'address',
-    skype: 'skype'
+    skype: 'skype',
   },
   regular: {
-    name: [
-      /([A-Z][a-z]*)(\s[A-Z][a-z]*)/
-    ],
-    email: [
-      /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/
-    ],
-    phone: [
-      /((?:\+?\d{1,3}[\s-])?\(?\d{2,3}\)?[\s.-]?\d{3}[\s.-]\d{4,5})/
-    ]
-  }
+    name: [/([A-Z][a-z]*)(\s[A-Z][a-z]*)/],
+    email: [/([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/],
+    phone: [/((?:\+?\d{1,3}[\s-])?\(?\d{2,3}\)?[\s.-]?\d{3}[\s.-]\d{4,5})/],
+  },
 };
 
 // helper method
 function download(url, callback) {
-  request(url, function (error, response, body) {
+  request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       callback(body);
     } else {
-      callback(null, error)
+      callback(null, error);
     }
   });
 }
